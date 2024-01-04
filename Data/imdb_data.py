6e239bbf66b8872849f288a91455801e17caccb8
Esvararajaa <142655605+Esvararajaa@users.noreply.github.com>
2024-01-04 10:04:24 +0530

    Delete Data/imdb_data.py

--- Data/imdb_data.py
+++
@@ -1,9 +0,0 @@
-class data:
-    name = "Uttam Kumar"
-    birth_from = "01-01-1920"
-    birth_to = "31-12-1927"
-    month_date = "09-03"
-    eg_input = "Greatest Actor"
-    death_from = "01-01-1980"
-    death_to = "31-12-1980"
-    credit = "awesome"
